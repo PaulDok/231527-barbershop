559248f41d1c61bc124da87f14827b1527bd9aa3
PaulDok <pauldok@rambler.ru>
2017-02-08 22:20:37 +0300

    Завершаем вёрстку «Барбершопа»

---
+++ js/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var loginButton = document.querySelector('a.login');
+var loginCloseButton = document.querySelector('.modal-content .modal-content-close');
+var mapOpenButton = document.querySelector('.map-open-button');
+var mapCloseButton = document.querySelector('.modal-content-map .modal-content-close');
+
+var loginOverlay = document.querySelector('.modal-content');
+var mapOverlay = document.querySelector('.modal-content-map');
+
+var showLogin = function () {
+  loginOverlay.style['display'] = 'block';
+};
+
+var hideLogin = function () {
+  loginOverlay.style['display'] = 'none';
+};
+
+var showMap = function () {
+  mapOverlay.style['display'] = 'block';
+};
+
+var hideMap = function () {
+  mapOverlay.style['display'] = 'none';
+};
+
+loginButton.addEventListener('click', showLogin);
+loginCloseButton.addEventListener('click', hideLogin);
+mapOpenButton.addEventListener('click', showMap);
+mapCloseButton.addEventListener('click', hideMap);
